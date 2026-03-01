1cf80a81feff556c96ea574fd7f2344864352fa4
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2026-03-01 13:32:11 -0500

    feat: configure monorepo tester with workingDirectory support
    - Projen at root generates workflows with defaults.run.working-directory: infra
    - CDK app in infra/ with SNS topic + Lambda IAM role test stacks
    - Real AWS accounts (dev: 590183750202, prod: 471112928747)
    - IAM templates for changeset and drift detection roles
    - Workflows use $GITHUB_WORKSPACE prefix for script paths

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,64 +1,190 @@
-import { awscdk } from 'projen';
+import {
+  CdkDiffIamTemplate,
+  CdkDiffStackWorkflow,
+  CdkDriftDetectionWorkflow,
+  CdkDriftIamTemplate,
+} from '@jjrawlins/cdk-diff-pr-github-action';
+import { awscdk, TextFile } from 'projen';
 import { GithubCredentials } from 'projen/lib/github';
-import { CdkDiffStackWorkflow, CdkDriftDetectionWorkflow } from '@jjrawlins/cdk-diff-pr-github-action';
+
+const Environments = {
+  devops: { account: '891377240835', region: 'us-east-2' },
+  dev: { account: '590183750202', region: 'us-east-2' },
+  prod: { account: '471112928747', region: 'us-east-2' },
+};
+
+const workflowNodeVersion = '24.x';
 
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.189.1',
   defaultReleaseBranch: 'main',
   name: 'cdk-diff-pr-github-action-tester-monorepo',
+  minNodeVersion: '20.0.0',
   projenrcTs: true,
-  github: true,
+  workflowBootstrapSteps: [
+    {
+      name: 'configure aws credentials',
+      uses: 'aws-actions/configure-aws-credentials@v4',
+      with: {
+        'role-to-assume': '${{ secrets.AWS_GITHUB_OIDC_ROLE }}',
+        'role-duration-seconds': 900,
+        'aws-region': '${{ secrets.AWS_GITHUB_OIDC_REGION }}',
+        'role-skip-session-tagging': true,
+        'role-session-name': 'GitHubActions',
+      },
+    },
+  ],
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      projenCredentials: GithubCredentials.fromApp({
+        appIdSecret: 'PROJEN_APP_ID',
+        privateKeySecret: 'PROJEN_APP_PRIVATE_KEY',
+      }),
+    },
+  },
+  deps: [
+    '@jjrawlins/cdk-diff-pr-github-action@*',
+  ],
+  devDeps: [
+    '@stylistic/eslint-plugin@^5',
+  ],
+  tsconfig: {
+    compilerOptions: {
+      strict: true,
+      strictPropertyInitialization: false,
+      rootDir: '.',
+    },
+  },
+  tsconfigDev: {
+    compilerOptions: {
+      strict: true,
+      strictPropertyInitialization: false,
+      rootDir: '.',
+    },
+  },
   githubOptions: {
     projenCredentials: GithubCredentials.fromApp({
       appIdSecret: 'PROJEN_APP_ID',
       privateKeySecret: 'PROJEN_APP_PRIVATE_KEY',
     }),
+    mergify: false,
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: [
+          'feat', 'fix', 'docs', 'style', 'refactor',
+          'perf', 'test', 'chore', 'revert', 'ci',
+        ],
+      },
+    },
   },
-  deps: [
-    '@jjrawlins/cdk-diff-pr-github-action',
+  gitignore: [
+    '.env',
+    'infra/node_modules',
+    'infra/cdk.out',
+    'infra/lib',
   ],
 });
 
-// ---- CDK Diff Workflows (with workingDirectory) ----
+// ---- CDK Diff Workflows (with workingDirectory: 'infra') ----
 
 new CdkDiffStackWorkflow({
   project,
   workingDirectory: 'infra',
-  oidcRoleArn: 'arn:aws:iam::111122223333:role/GitHubOidcRole',
-  oidcRegion: 'us-east-1',
+  oidcRoleArn: `arn:aws:iam::${Environments.devops.account}:role/GitHubActionsOidcRoleDevOps`,
+  oidcRegion: Environments.devops.region,
   stacks: [
     {
       stackName: 'MonorepoTestStack-dev',
-      changesetRoleToAssumeArn: 'arn:aws:iam::111122223333:role/cdk-diff-workflow-iam-role',
-      changesetRoleToAssumeRegion: 'us-east-1',
+      changesetRoleToAssumeArn: `arn:aws:iam::${Environments.dev.account}:role/cdk-diff-workflow-iam-role`,
+      changesetRoleToAssumeRegion: Environments.dev.region,
     },
     {
       stackName: 'MonorepoTestStack-prod',
-      changesetRoleToAssumeArn: 'arn:aws:iam::444455556666:role/cdk-diff-workflow-iam-role',
-      changesetRoleToAssumeRegion: 'us-east-1',
+      changesetRoleToAssumeArn: `arn:aws:iam::${Environments.prod.account}:role/cdk-diff-workflow-iam-role`,
+      changesetRoleToAssumeRegion: Environments.prod.region,
     },
   ],
+  nodeVersion: workflowNodeVersion,
 });
 
-// ---- CDK Drift Detection Workflow (with workingDirectory) ----
+// ---- CDK Drift Detection Workflow (with workingDirectory: 'infra') ----
 
 new CdkDriftDetectionWorkflow({
   project,
   workingDirectory: 'infra',
-  oidcRoleArn: 'arn:aws:iam::111122223333:role/GitHubOidcRole',
-  oidcRegion: 'us-east-1',
-  schedule: '0 6 * * 1', // Every Monday at 6am UTC
+  oidcRoleArn: `arn:aws:iam::${Environments.devops.account}:role/GitHubActionsOidcRoleDevOps`,
+  oidcRegion: Environments.devops.region,
   stacks: [
     {
       stackName: 'MonorepoTestStack-dev',
-      driftDetectionRoleToAssumeArn: 'arn:aws:iam::111122223333:role/cdk-drift-detection-role',
-      driftDetectionRoleToAssumeRegion: 'us-east-1',
+      driftDetectionRoleToAssumeArn: `arn:aws:iam::${Environments.dev.account}:role/cdk-drift-workflow-iam-role`,
+      driftDetectionRoleToAssumeRegion: Environments.dev.region,
     },
     {
       stackName: 'MonorepoTestStack-prod',
-      driftDetectionRoleToAssumeArn: 'arn:aws:iam::444455556666:role/cdk-drift-detection-role',
-      driftDetectionRoleToAssumeRegion: 'us-east-1',
+      driftDetectionRoleToAssumeArn: `arn:aws:iam::${Environments.prod.account}:role/cdk-drift-workflow-iam-role`,
+      driftDetectionRoleToAssumeRegion: Environments.prod.region,
     },
+  ],
+  nodeVersion: workflowNodeVersion,
+});
+
+// ---- IAM Templates ----
+
+new CdkDiffIamTemplate({
+  project,
+  outputPath: 'cdk-diff-workflow-iam-template.yaml',
+  oidcRegion: Environments.devops.region,
+  oidcRoleArn: `arn:aws:iam::${Environments.devops.account}:role/GitHubActionsOidcRoleDevOps`,
+  roleName: 'cdk-diff-workflow-iam-role',
+});
+
+new CdkDriftIamTemplate({
+  project,
+  outputPath: 'cdk-drift-workflow-iam-template.yaml',
+  oidcRegion: Environments.devops.region,
+  oidcRoleArn: `arn:aws:iam::${Environments.devops.account}:role/GitHubActionsOidcRoleDevOps`,
+  roleName: 'cdk-drift-workflow-iam-role',
+});
+
+// ---- Workflow overrides ----
+
+project.github!.tryFindWorkflow('build')!.file!.addOverride('jobs.build.permissions.id-token', 'write');
+project.github!.tryFindWorkflow('build')!.file!.addOverride('jobs.build.permissions.packages', 'read');
+project.github!.tryFindWorkflow('build')!.file!.addOverride('jobs.build.env.GITHUB_TOKEN', '${{ secrets.GITHUB_TOKEN }}');
+
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.upgrade.permissions.id-token', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.upgrade.permissions.packages', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.upgrade.permissions.pull-requests', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.upgrade.permissions.contents', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.upgrade.env.GITHUB_TOKEN', '${{ secrets.GITHUB_TOKEN }}');
+
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.pr.permissions.id-token', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.pr.permissions.packages', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.pr.permissions.pull-requests', 'write');
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.pr.permissions.contents', 'write');
+
+project.github!.tryFindWorkflow('upgrade')!.file!.addOverride('jobs.pr.steps.6', {
+  name: 'Enable auto-merge',
+  if: "steps.create-pr.outputs.pull-request-number != ''",
+  run: 'gh pr merge --auto --squash "${{ steps.create-pr.outputs.pull-request-number }}"',
+  env: {
+    GH_TOKEN: '${{ steps.generate_token.outputs.token }}',
+  },
+});
+
+project.package.addField('resolutions', {
+  '@stylistic/eslint-plugin': '^5',
+});
+
+project.tryRemoveFile('.npmrc');
+new TextFile(project, '.npmrc', {
+  lines: [
+    '# ~~ Generated by projen. To modify, edit .projenrc.ts and run "npx projen".',
+    '',
+    '@JaysonRawlins:registry=https://npm.pkg.github.com',
+    '//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}',
   ],
 });
 
--- infra/src/main.ts
+++ infra/src/main.ts
@@ -1,12 +1,30 @@
-import { App, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 
 class MonorepoTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CfnOutput(this, 'Message', {
-      value: `Hello from ${id}`,
+    const topic = new Topic(this, 'TestTopic');
+
+    new Role(this, 'TestLambdaRole', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        SNSPublish: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: ['sns:Publish'],
+              resources: [topic.topicArn],
+            }),
+          ],
+        }),
+      },
+    });
+
+    new CfnOutput(this, 'TopicArn', {
+      value: topic.topicArn,
     });
   }
 }
@@ -14,11 +32,11 @@ class MonorepoTestStack extends Stack {
 const app = new App();
 
 new MonorepoTestStack(app, 'MonorepoTestStack-dev', {
-  env: { account: '111122223333', region: 'us-east-1' },
+  env: { account: '590183750202', region: 'us-east-2' },
 });
 
 new MonorepoTestStack(app, 'MonorepoTestStack-prod', {
-  env: { account: '444455556666', region: 'us-east-1' },
+  env: { account: '471112928747', region: 'us-east-2' },
 });
 
 app.synth();
